6243d21dd4cc4372c82713dd15f945ea38d989e0
Ivory <IvoryEC@gmail.com>
2023-05-15 22:23:23 -0400

    tidy up, remove unused code, add iteration

--- code/ivory/decontaminate_SCRuB.R
+++ code/ivory/decontaminate_SCRuB.R
@@ -9,15 +9,9 @@ library(vegan)
 library(glmnet)
 library(torch)
 install_torch()
+library(SCRuB)
 
 sessionInfo()
-
-seed=3
-## Do several independent iterations. For now, just do it all once.
-# for(seed in 0:2){
-
-set.seed(seed)
-message(paste('running using seed:', seed))
 
 suppressWarnings({
     dir.create("../results")
@@ -37,6 +31,9 @@ dim(unique_samps)
 unique_metadata <- read.csv("../results/data/ivory/before-decontamination/unique_metadata.csv", row.names = 1)
 message("unique_metadata.csv has dimentions:")
 dim(unique_metadata)
+
+# These lines were not needed when using R version 4.2.2 (2022-10-31), 
+# but without them I hit errors when using R version 3.6.3 (2020-02-29)
 unique_metadata$sample_plate = as.character(unique_metadata$sample_plate)
 unique_metadata$sample_type = as.character(unique_metadata$sample_type)
 unique_metadata$sample_well = as.character(unique_metadata$sample_well)
@@ -48,13 +45,24 @@ unique_metadata$sample_well = as.character(unique_metadata$sample_well)
 
 #### run through SCRuB ####
 
-useRPackage = T
-if (useRPackage){
-    library(SCRuB)
-    message("SCRuB package version: ", packageVersion("SCRuB"))
+numTrials = 10
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) > 0) numTrials = args[1]
+
+for (trialNumber in 1:numTrials){
+    message("Trial ", trialNumber, " of ", numTrials)
     
+    seed = trialNumber * 29
+    message(paste('Using seed:', seed))
+    set.seed(seed)
+
+    # I thought I would need to run SCRuB for each contamination source, 
+    # but it looks like SCRuB internalizes that iteration process.
     control_types = c('control blank library prep', 'control blank DNA extraction')
     unique_metadata$is_control = unique_metadata$sample_type %in% control_types
+    
+    # Note: This decontaminates per plate. And one plate, is not decontaminated all. But the well location info is optional.  
+    #       Could I instead decontaminate the whole data set as one if I just ignore the well location info?
     
     plates = unique(unique_metadata$sample_plate)
     message("SCRuB the ", length(plates), " plates separately.")
@@ -71,7 +79,7 @@ if (useRPackage){
         
         
         # when I passed a table with no controls, got error: "Error in rowSums(.) : 'x' must be numeric"
-        # so skip those plates
+        # so skip any plate with 0 controls
         numControls = sum(plate_meta$is_control)
         message("Plate has controls: ", numControls)
         message(paste(row.names(plate_meta)[plate_meta$is_control], collapse=", "))
@@ -81,10 +89,7 @@ if (useRPackage){
             
             # decontaminated data
             scrub_out_list[[plate]] <- scrub_output$decontaminated_samples
-
-            # I don't understand this error: "Error: value for ‘self’ not found"
-            # happened to plate CMI_Plasma_12692_P4 -- but not the following time. (?)
-            # only happens some of the time.
+            
         }else{
             message("Skipping plate")
             scrub_out_list[[plate]] <- plate_data
@@ -92,78 +97,21 @@ if (useRPackage){
     }
     scrub_df = do.call("rbind", scrub_out_list)
     
-}else{
-    source('SCRuB/lsq_initializations.R')
-    source('SCRuB/spatial_functions.R')
-    source('SCRuB/main_functions.R')
+    message("scrub_df has dimensions:")
+    dim(scrub_df)
     
-    well_dists <- unique_metadata %>%
-        mutate(well_loc= sample_well %>% substr(1,1) %>% sapply( function(x) which(LETTERS==x)[1]), 
-               indices_loc = sample_well%>% substr(2,3) %>% as.integer ) %>%
-        select(well_loc, indices_loc) %>%
-        dist(method = 'euclidean') %>% as.matrix()
+    message("Filtering down to columns with sums > 500")
+    scrub_df <- scrub_df[, (colSums(scrub_df) > 500) %>% which]
     
-    well_dists = round(well_dists, digits = 2)
+    message("scrub_df has dimensions:")
+    dim(scrub_df)
     
-    
-    tmp_fwd <- as.matrix( unique_samps[ unique_metadata %>% 
-                                            filter(sample_type %>% 
-                                                       str_detect('control blank library prep') == F) %>% 
-                                            pull(X.SampleID) %>% 
-                                            as.character(), ] )
-    
-    ######## encounters problem here
-    plasma_scrubbed <- spatial_SCRUB(data=tmp_fwd, 
-                                     is_control = unique_metadata[row.names(tmp_fwd),]$sample_type=='control blank DNA extraction',
-                                     well_dists = well_dists, 
-                                     dist_threshold =1.5
-    )
-    
-    Qrow.names( plasma_scrubbed$decontaminated_samples ) <- unique_metadata[row.names(tmp_fwd),
-    ][ unique_metadata[row.names(tmp_fwd),
-    ]$sample_type!='control blank DNA extraction', ] %>% row.names()
-    
-    
-    
-    next_lvl_mat <- plasma_scrubbed$decontaminated_samples %>%
-        rbind(unique_samps[row.names(unique_metadata %>%
-                                         filter(sample_type=='control blank library prep')), ])
-    
-    
-    
-    plasma_scrubbed_2nd <- spatial_SCRUB(data=next_lvl_mat, 
-                                         is_control = c( rep(F, nrow(plasma_scrubbed$decontaminated_samples) ),
-                                                         rep(T, sum(unique_metadata$sample_type=='control blank library prep'))),
-                                         well_dists = well_dists,
-                                         dist_threshold =  1.5 )
-    
-    
-    row.names(plasma_scrubbed_2nd$decontaminated_samples) <- unique_metadata[row.names(plasma_scrubbed$decontaminated_samples),
-    ][ unique_metadata[row.names(plasma_scrubbed$decontaminated_samples ),
-    ]$sample_type!='control blank library prep', ] %>% row.names()
-    
-    
-    colnames(plasma_scrubbed_2nd$decontaminated_samples) <- colnames(unique_samps)
-    
-    scrub_df <- plasma_scrubbed_2nd$decontaminated_samples[,  ( ( plasma_scrubbed_2nd$decontaminated_samples %>% colSums() ) > 0 ) %>% which]
-    scrub_df <- scrub_df[row.names(metadataPSMatchedDPQCFiltered), ]
-    
+    resDir = paste0("../results/data/ivory/decontamination/SCRuB/trial_", trialNumber)
+    suppressWarnings({dir.create(resDir)})
+    file = file.path(resDir, "scrub_output.csv")
+    message("Saving scrubbed data as: ", file)
+    write.csv(scrub_df, file)
     
 }
 
-
-
-message("scrub_df has dimensions:")
-dim(scrub_df)
-
-message("Filtering down to columns with sums > 500")
-scrub_df <- scrub_df[, (colSums(scrub_df) > 500) %>% which]
-
-message("scrub_df has dimensions:")
-dim(scrub_df)
-
-# trialNumber=seed
-# file=paste0("../results/data/ivory/decontamination/SCRuB/trial_", trialNumber, "/scrub_output.csv")
-file="../results/data/ivory/decontamination/SCRuB/scrub_output.csv"
-message("Saving scrubbed data as: ", file)
-write.csv(scrub_df, file)
+message("Done!")
