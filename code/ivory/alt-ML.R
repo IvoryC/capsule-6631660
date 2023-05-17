77bec9780b293a118e3f2d6ba84ab624fca93686
Ivory <IvoryEC@gmail.com>
2023-05-17 17:22:33 -0400

    Add loop to go through many leave-on-out samples, and create a summary of results for the input file.

--- code/ivory/alt-ML.R
+++ code/ivory/alt-ML.R
@@ -26,147 +26,188 @@ sessionInfo()
 
 metadataPSMatchedDPQCFiltered <- read.csv('../data/Fig4_plasma/Metadata-Plasma-Filtered-For-Analysis.csv', row.names=1)
 
-#### find data files ####
+#### find input data files ####
 
 decontaminationFolder = "../results/data/ivory/decontamination"
 inputPattern = "_vsnm.csv"
 infiles = dir(decontaminationFolder, pattern=inputPattern, full.names = T, recursive = T)
 
-# create directory for output
-predictionFolder = "../results/data/ivory/prediction"
-suppressWarnings(dir.create(predictionFolder))
-
 ## raed data --- later do this in loop
-infile = infiles[3]
-message("Reading data from file: ", infile)
-data = read.csv(infile, row.names = 1)
+infiles = infiles[3]
 
-#### train ####
+for (infile in infiles){
+    
+    message("Reading data from file: ", infile)
+    data = read.csv(infile, row.names = 1)
+    
+    # save output with matching sub-directory structure
+    predictionFolderBase = "../results/data/ivory/prediction"
+    indir = dirname(infile)
+    predictionFolder = sub(decontaminationFolder, predictionFolderBase, indir)
+    suppressWarnings(dir.create(predictionFolder, recursive = T))
 
-# pick one ---later do this as a loop through all samples.
-test1 = sample(row.names(data), size=1)
-message("Leave out sample ", test1, " as the test.")
+    # Save all images for this input file to one pdf
+    plotFile = file.path(predictionFolder, sub(inputPattern, "_leave-1-out-plots.pdf", basename(infile)))
+    pdf(plotFile)
+    
+    fileSummary = data.frame()
+    
+    #### select test data ####
+    
+    categories = c("Control", "SKCM")
+    hasCat = metadataPSMatchedDPQCFiltered$disease_type_consol %in% categories
+    nameHasCat = row.names(metadataPSMatchedDPQCFiltered)[which(hasCat)]
+    
+    for (test1 in nameHasCat){
+    
+    
+    
+    # pick one ---later do this as a loop through all samples.
+    #test1 = sample(row.names(data), size=1)
+    message("Leave out sample ", test1, " as the test.")
+    
+    train = filter(data, rownames(data) != test1)
+    
 
-train = filter(data, rownames(data) != test1)
-
-categories = c("Control", "SKCM")
-
-hasCat = metadataPSMatchedDPQCFiltered$disease_type_consol %in% categories
-nameHasCat = row.names(metadataPSMatchedDPQCFiltered)[which(hasCat)]
-
-# interest point... is it better to train on only the categories of interest? or on all data?
-# datasub = data[nameHasCat, ]
-
-##### prcomp #####
-
-res = stats::prcomp(train)
-
-axes = res$x
-
-pcDF = merge(axes, metadataPSMatchedDPQCFiltered, by=0)
-row.names(pcDF) = pcDF$Row.names
-pcDF = pcDF %>% select(-Row.names)
-
-#### classic biplots
-#
-# plot1 = ggplot2::ggplot(data=pcDF[nameHasCat,]) +
-#     geom_point(mapping = aes(x=PC1, y=PC2, color = disease_type)) +
-#     ggtitle("prcomp")
-# plot1
-# 
-# plot2 = ggplot2::ggplot(data=pcDF[nameHasCat,]) +
-#     geom_point(mapping = aes(x=PC3, y=PC4, color = disease_type)) +
-#     ggtitle("prcomp")
-# plot2
-
-axisSet = colnames(axes)
-if (length(axisSet) > 12) axisSet = axisSet[1:12]
-
-d4 = pcDF %>% 
-    filter(disease_type_consol %in% categories) %>% 
-    select(axisSet, disease_type_consol) %>% 
-    gather("value", key="PC", -disease_type_consol) %>%
-    mutate(axisNum = as.numeric(gsub("PC", "", PC))) %>%
-    arrange(axisNum) 
-
-# reset the factor levels so that the plots appear in order
-d5 = d4 %>%
-    arrange(axisNum) %>%
-    mutate(PC = factor(PC, levels=unique(PC))) %>%
-    select(-axisNum)
-
-# plot3 = ggplot2::ggplot(data=d4) +
-#     geom_boxplot(mapping = aes(x=Axis, y=value, color = disease_type_consol)) +
-#     ggtitle("PCoA values split by sample type")
-# plot3
-
-plot4 = ggplot2::ggplot(data=d5) +
-    geom_boxplot(mapping = aes(y=value, x=disease_type_consol, color = disease_type_consol)) +
-    ggtitle("prcomp values split by sample type") +
-    facet_wrap(~PC, nrow=3)
-#plot4
-
-##### prcomp choose best #####
-# Choose the single best axis for separating the data.
-axisPs = sapply(axisSet, function(ax) {
-    t.test(data=d4 %>% filter(PC == ax), 
-           value ~ disease_type_consol)$p.value
-})
-bestAxis = names(axisPs)[which(axisPs == min(axisPs))]
-message("Axis ", bestAxis, " is the best at separating ", paste(categories, collapse=" from "))
-
-# highlight the best axis in the plot
-plot6 = plot4 +
-    geom_rect(data = subset(d5, PC == bestAxis), 
-              fill = NA, colour = "red", 
-              xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf)
-plot6
-plotFile = sub(inputPattern, "_prcomp-boxplot.png", infile)
-ggsave(plotFile, plot=plot6)
-
-#### test sample ####
-
-###### sanity check ######
-
-# from https://rdrr.io/github/nchlis/pca.utils/man/project_pca.html
-# install.packages("remotes")
-# remotes::install_github("nchlis/pca.utils")
-#
-# example: pca.utils::project_pca(Xnew = NULL, pc = NULL)
-pro.check = project_pca(Xnew = data[train1,], pc = res)
-pro.check[,"PC1"] == res$x[,"PC1"][train1]
-# TRUE #--yay!
-
-### the project_pca function is very straight forward:
-# function (Xnew = NULL, pc = NULL) 
-# {
-#     return(scale(Xnew, pc$center, pc$scale) %*% pc$rotation)
-# }
-
-###### actual test ######
-
-pro.test = pca.utils::project_pca(Xnew = data[test1,], pc = res)
-pro.test.val = pro.test[,bestAxis]
-
-
-claims = sapply(categories, function(cat){
-    catVals = pcDF %>% 
-        filter(disease_type_consol == cat) %>%
-        select(bestAxis) %>% 
-        unlist()
-    t.test(catVals, pro.test.val, var.equal = T)$p.value
-})
-claims = claims[order(claims, decreasing = T)]
-
-# which category had the highest p? that's our prediction.
-predict = names(claims)[1]
-confidenceP = claims[2]
-
-realCategory = metadataPSMatchedDPQCFiltered[test1,"disease_type_consol"]
-isCorrect = predict == realCategory
-correctOrNot = ifelse(isCorrect, "CORRECT", paste0("WRONG (is actually ", realCategory, ")"))
-
-message("We predict (with confidence p=", round(confidenceP,3),") that sample [", test1, "] belongs in category [", predict, "], which is ", correctOrNot, "." )
-
+    #### train ####
+    
+    # interest point... is it better to train on only the categories of interest? or on all data?
+    # datasub = data[nameHasCat, ]
+    
+    ##### prcomp #####
+    
+    res = stats::prcomp(train)
+    
+    axes = res$x
+    
+    pcDF = merge(axes, metadataPSMatchedDPQCFiltered, by=0)
+    row.names(pcDF) = pcDF$Row.names
+    pcDF = pcDF %>% select(-Row.names)
+    
+    #### classic biplots
+    #
+    # plot1 = ggplot2::ggplot(data=pcDF[nameHasCat,]) +
+    #     geom_point(mapping = aes(x=PC1, y=PC2, color = disease_type)) +
+    #     ggtitle("prcomp")
+    # plot1
+    # 
+    # plot2 = ggplot2::ggplot(data=pcDF[nameHasCat,]) +
+    #     geom_point(mapping = aes(x=PC3, y=PC4, color = disease_type)) +
+    #     ggtitle("prcomp")
+    # plot2
+    
+    axisSet = colnames(axes)
+    if (length(axisSet) > 12) axisSet = axisSet[1:12]
+    
+    d4 = pcDF %>% 
+        filter(disease_type_consol %in% categories) %>% 
+        select(axisSet, disease_type_consol) %>% 
+        gather("value", key="PC", -disease_type_consol) %>%
+        mutate(axisNum = as.numeric(gsub("PC", "", PC))) %>%
+        arrange(axisNum) 
+    
+    # reset the factor levels so that the plots appear in order
+    d5 = d4 %>%
+        arrange(axisNum) %>%
+        mutate(PC = factor(PC, levels=unique(PC))) %>%
+        select(-axisNum)
+    
+    plot4 = ggplot2::ggplot(data=d5) +
+        geom_boxplot(mapping = aes(y=value, x=disease_type_consol, color = disease_type_consol)) +
+        ggtitle("prcomp values split by sample type", subtitle = paste0("made without sample: ", test1)) +
+        facet_wrap(~PC, nrow=3)
+    #plot4
+    
+    ##### prcomp choose best #####
+    # Choose the single best axis for separating the data.
+    axisPs = sapply(axisSet, function(ax) {
+        t.test(data=d4 %>% filter(PC == ax), 
+               value ~ disease_type_consol)$p.value
+    })
+    bestAxis = names(axisPs)[which(axisPs == min(axisPs))]
+    message("Axis ", bestAxis, " is the best at separating ", paste(categories, collapse=" from "))
+    
+    # highlight the best axis in the plot
+    plot6 = plot4 +
+        geom_rect(data = subset(d5, PC == bestAxis), 
+                  fill = NA, colour = "red", 
+                  xmin = -Inf,xmax = Inf, ymin = -Inf,ymax = Inf)
+    
+    # save plot
+    #plotFile = sub(inputPattern, "_prcomp-boxplot.png", infile)
+    #ggsave(plotFile, plot=plot6)
+    # printed images are directed to pdf
+    print(plot6)
+    
+    
+    #### test sample ####
+    
+    ###### sanity check ######
+    
+    # from https://rdrr.io/github/nchlis/pca.utils/man/project_pca.html
+    # install.packages("remotes")
+    # remotes::install_github("nchlis/pca.utils")
+    #
+    # example: pca.utils::project_pca(Xnew = NULL, pc = NULL)
+    train1 = row.names(train)[1]
+    pro.check = project_pca(Xnew = data[train1,], pc = res)
+    sanity = pro.check[,"PC1"] == res$x[,"PC1"][train1]
+    # TRUE #--yay!
+    if (!sanity) warning("Oh no! we might not be sane! Check the project_pca method.")
+    
+    ### the project_pca function is very straight forward:
+    # function (Xnew = NULL, pc = NULL) 
+    # {
+    #     return(scale(Xnew, pc$center, pc$scale) %*% pc$rotation)
+    # }
+    
+    ###### actual test ######
+    
+    pro.test = pca.utils::project_pca(Xnew = data[test1,], pc = res)
+    pro.test.val = pro.test[,bestAxis]
+    
+    
+    claims = sapply(categories, function(cat){
+        catVals = pcDF %>% 
+            filter(disease_type_consol == cat) %>%
+            select(bestAxis) %>% 
+            unlist()
+        t.test(catVals, pro.test.val, var.equal = T)$p.value
+    })
+    claims = claims[order(claims, decreasing = T)]
+    
+    # which category had the highest p? that's our prediction.
+    predict = names(claims)[1]
+    confidenceP = claims[2]
+    
+    realCategory = metadataPSMatchedDPQCFiltered[test1,"disease_type_consol"]
+    isCorrect = predict == realCategory
+    correctOrNot = ifelse(isCorrect, "CORRECT", paste0("WRONG (is actually ", realCategory, ")"))
+    
+    message("We predict (with confidence p=", round(confidenceP,3),") that sample [", test1, "] belongs in category [", predict, "], which is ", correctOrNot, "." )
+    
+    
+    
+    fileSummary = rbind(fileSummary, 
+                        c(sampleID=test1, actual=realCategory, prediction=predict, confidenceP=confidenceP))
+    
+    } #done test with sample
+    
+    names(fileSummary) = c("sampleID", "actual", "prediction", "confidenceP")
+    fileSummary$isCorrect = fileSummary$actual == fileSummary$prediction
+    summr = paste("Predictions were currect for", sum(fileSummary$isCorrect), "of", nrow(fileSummary), "leave-1-out samples.")
+    message(summr)
+    
+    tableFile = file.path(predictionFolder, sub(inputPattern, "_leave-1-out-prediction-summary.txt", basename(infile)))
+    message("Saving file: ", tableFile)
+    writeLines(tableFile, text = paste("#", summr))
+    write.table(fileSummary, file=tableFile, quote=F, row.names = F, sep="\t", append=T)
+    rm(fileSummary)
+    
+    dev.off()
+    
+    message("Done with file ", infile, ".")
+    
+}
 
 message("Done!")
