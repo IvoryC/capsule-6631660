91bd7ac063e01685f528c65208bdc1a1de19c94a
Ivory <IvoryEC@gmail.com>
2023-05-18 10:24:57 -0400

    Add command options to reduce blanks in decontaminate_noPlate-SCRuB.R

--- code/ivory/decontaminate_noPlate-SCRuB.R
+++ code/ivory/decontaminate_noPlate-SCRuB.R
@@ -1,5 +1,13 @@
 
+#### notes ####
 # This draft started by taking the first 334 lines of Run_plastma_deconts_and_preds.R
+
+# I thought I would need to run SCRuB for each contamination source, 
+# but it looks like SCRuB internalizes that iteration process.
+
+# Note: This ignores plate and plate location to instead handle all samples in one go.
+
+#### libraries ####
 
 # install.packages('splitstackshape', repos='http://cran.us.r-project.org')
 # library(splitstackshape)
@@ -13,23 +21,34 @@ library(SCRuB)
 
 sessionInfo()
 
-suppressWarnings({
-    dir.create("../results")
-    dir.create("../results/data")
-    dir.create("../results/data/ivory")
-    dir.create("../results/data/ivory/decontamination")
-    dir.create("../results/data/ivory/decontamination/SCRuB_noPlate")
-})
+#### read args ####
+
+# defaults
+numTrials = 2 # the number of times to run scrub and produce a result
+dropBlanks = .5 # 0-1, the proportion of blanks to drop
+
+# command line
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) > 0) {
+    args2 = args %>% strsplit(split="=")
+    key = sapply(args2, function(x) x[[1]])
+    argVals = sapply(args2, function(x) x[[2]])
+    names(argVals) = key
+    if ("trials" %in% key) numTrials = argVals["trials"]
+    if ("dropBlanks" %in% key) dropBlanks = argVals["dropBlanks"]
+}
+message("Will run ", numTrials, " trials.")
+message("In each trial, will subsample to remove proportion of blanks: ", dropBlanks)
 
 #### read data ####
 
 full_df = read.csv("../results/data/ivory/before-decontamination/full_df.csv", row.names = 1)
-message("full_df.csv has dimentions:")
+print("full_df.csv has dimentions:")
 dim(full_df)
 
 metadata <- read.csv('../data/Fig4_plasma/47212_47212_analysis_mapping.txt', sep='\t')
 row.names(metadata) = metadata$X.SampleID
-message("metadata has dimentions:")
+print("metadata has dimentions:")
 dim(metadata)
 
 # These lines were not needed when using R version 4.2.2 (2022-10-31), 
@@ -44,11 +63,10 @@ sample_intersect = intersect(row.names(metadata),
 counts = full_df[sample_intersect, ]
 meta = metadata[sample_intersect, ]
 
-#### run through SCRuB ####
+mainResultsDir = "../results/data/ivory/decontamination"
+message("Results will be saved under: ", mainResultsDir)
 
-numTrials = 2
-args = commandArgs(trailingOnly=TRUE)
-if (length(args) > 0) numTrials = args[1]
+#### run through SCRuB ####
 
 for (trialNumber in 1:numTrials){
     message("Trial ", trialNumber, " of ", numTrials)
@@ -57,12 +75,26 @@ for (trialNumber in 1:numTrials){
     message(paste('Using seed:', seed))
     set.seed(seed)
     
-    # I thought I would need to run SCRuB for each contamination source, 
-    # but it looks like SCRuB internalizes that iteration process.
     control_types = c('control blank library prep', 'control blank DNA extraction')
     meta$is_control = meta$sample_type %in% control_types
     
-    # Note: This ignores plate and plat location to instead handle all samples in one go.
+    
+    if (dropBlanks > 0){
+        totalBlanks = sum(meta$is_control)
+        # use ceiling, so if there is at least one blank in the data, 
+        # you are guaranteed to have at least one blank to process with.
+        numKeepBlanks = ceiling(totalBlanks * (1 - dropBlanks))
+        message("Onlye use ", numKeepBlanks, " blanks (out of ", totalBlanks, ")." )
+        # split out the controls and sub-sample them, then put back with samples 
+        metaSp = split(meta, f=meta$is_control)
+        metaSp[['TRUE']] = metaSp[['TRUE']][ sample(1:totalBlanks, size=numKeepBlanks, replace=F), ]
+        meta = rbind(metaSp[[1]], metaSp[[2]])
+        # subset counts to match meta
+        counts = counts[row.names(meta), ]
+    }else{
+        message("Keeping all blanks.")
+    }
+    
     
     # when I passed a table with no controls, got error: "Error in rowSums(.) : 'x' must be numeric"
     numControls = sum(meta$is_control)
@@ -82,8 +114,10 @@ for (trialNumber in 1:numTrials){
     message("scrub_df has dimensions:")
     dim(scrub_df)
     
-    resDir = paste0("../results/data/ivory/decontamination/SCRuB_noPlate/trial_", trialNumber)
-    suppressWarnings({dir.create(resDir)})
+    numBlanks = sum(meta$is_control)
+    resDir = file.path(mainResultsDir, paste0("SCRuB_noPlate_", numBlanks, "-blanks"), paste0("trial_", trialNumber))
+    #resDir = paste0("../results/data/ivory/decontamination/SCRuB_noPlate_", numBlanks, "-blanks/trial_", trialNumber)
+    suppressWarnings({dir.create(resDir, recursive = T)})
     file = file.path(resDir, "scrub_decontaminated.csv")
     message("Saving scrubbed data as: ", file)
     write.csv(scrub_df, file)
